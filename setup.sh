b0feb701be75dc4b86877b3344788151cf1484fc
Phantom Code | Cyber Reign <ahadpythonprogrammer@gmail.com>
2025-06-05 13:25:07 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+echo "───────────────────────────────────────────────────────────────"
+echo " 🔥 Tool developed by Phantom-Code-CyberReign (Phantom Reign) "
+echo " ⚠️ THIS TOOL IS FOR EDUCATIONAL PURPOSES ONLY!"
+echo " 🚫 Unauthorized use for illegal activities is strictly prohibited."
+echo " 🌐 Visit: github.com/Phantom-Code-CyberReign"
+echo " 🏴 Based in Pakistan | Cybersecurity Research & Development"
+echo "───────────────────────────────────────────────────────────────"
+
+
 echo "[*] Killing interfering processes and enabling monitor mode..."
 sudo airmon-ng check kill
 sudo airmon-ng start wlan0
